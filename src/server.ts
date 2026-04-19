289375c6cdf7105e1504767c02b15b59cdfebb71
Bikash Kakati <bikashkakati796@gmail.com>
2026-04-19 11:04:54 +0530

    [FIX] Convert dot env execution  from functional based to import based that it import before runtime.

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,13 @@
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import { connectDB } from './config/db';
 import app from './app';
 import { DB_DETAILS } from './constant';
 
-dotenv.config();
-
 const PORT = process.env.SERVER_PORT || 4000;
 const { DB_URI, DB_NAME } = DB_DETAILS;
 
-if (require.main === module) {
-    connectDB(DB_URI, DB_NAME).then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
+connectDB(DB_URI, DB_NAME).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
     });
-}
+});

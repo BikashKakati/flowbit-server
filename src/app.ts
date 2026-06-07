35a94c4b956b3c44cf1d43145545300af7bf5bff
Bikash Kakati <bikashkakati796@gmail.com>
2026-06-07 18:56:55 +0530

    [ADD] Cors For local use

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,7 @@ import { errorHandler } from './middlewares/error.middleware';
 
 const app = express();
 
-// app.use(cors({
-
-// }));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
 import authRoutes from './routes/auth.routes';

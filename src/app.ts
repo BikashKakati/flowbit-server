a00a111823b632b4f4faf04a0bb8ae0af98b0af6
Bikash Kakati <bikashkakati796@gmail.com>
2026-06-07 18:50:55 +0530

    [UPDATE] Remove cors because we already had cors in lambda function url

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,9 @@ import { errorHandler } from './middlewares/error.middleware';
 
 const app = express();
 
-app.use(cors());
+// app.use(cors({
+
+// }));
 app.use(express.json({ limit: '10mb' }));
 
 import authRoutes from './routes/auth.routes';
